4ed6610847789b8ec7b8bbd53d9bf941df522f02
Monica Becraft <mbecraft1216@gmail.com>
2022-12-29 10:09:37 -0500

    added select functionality

--- src/app.js
+++ src/app.js
@@ -27,18 +27,21 @@ function updateTime() {
 }
 
 function updateCity(event) {
-  let cityTimezone = event.target.value;
-  if (cityTimezone === "current") {
-    cityTimezone = moment.tz.guess();
-  }
-  let cityName = cityTimezone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimezone);
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = ` <div class="city"> <div> <h2>${cityName}</h2> <div class="date">${cityTime.format(
-    "MMMM Do YYYY"
-  )}</div> </div> <div class="time">${cityTime.format(
-    "h:mm:ss:SSS"
-  )} <small>${cityTime.format("A")}</small></div></div>`;
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format(
+      "h:mm:ss:SSS"
+    )} <small>${cityTime.format("A")}</small></div>
+  </div>
+  `;
 }
 
 updateTime();
